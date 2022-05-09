fb45eb323d3022dd466419b79d0f866d9fffc04f
MaximeSchuybroeck <maxime.schuybroeck@student.kuleuven.be>
2022-05-09 18:15:20 +0200

    Maxime V15 | einde sessie

--- app/src/main/java/java/be/kuleuven/myapplication/LoginActivity.java
+++ app/src/main/java/java/be/kuleuven/myapplication/LoginActivity.java
@@ -1,13 +1,34 @@
 package java.be.kuleuven.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+
+    private UserProfile userProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    // to login en check with the database, then forwards you to the begin screen( the manage screen)
+    public void login(String username, String password){
+        // TODO: 2/05/2022 in database kijken of dit account bestaat
+    }
+
+    public void addUser(View view){
+        setContentView(R.layout.activity_new_user);
+    }
+
+    /*
+    Button addUser = (Button) findViewById(R.id.newUserButton);
+        addUser.setOnClickListener(new View.OnClickListener());
+
+     */
+
 }
