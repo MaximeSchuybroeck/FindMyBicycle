4331c4d979a0c6286291a03fc84680ca568d1c5d
LAPTOP-KOENRAAD\koenr <koenraad.goddefroy@hotmail.com>
2022-05-09 18:11:14 +0200

    geen idee wat er aan het gebeuren is

--- app/src/main/java/java/be/kuleuven/myapplication/BikeActivity.java
+++ app/src/main/java/java/be/kuleuven/myapplication/BikeActivity.java
@@ -2,12 +2,21 @@ package java.be.kuleuven.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BikeActivity extends Activity {
+    private UserProfile owner;
+    private Bike bike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bike);
     }
+    public void addBike(View view)
+    {
+        owner.addBikeList(bike);
+
+    }
 }
+
