f40343c3a1bfd8908c0c4712dfeb0e41c212b149
MaximeSchuybroeck <maxime.schuybroeck@student.kuleuven.be>
2022-05-10 22:10:37 +0200

    Maxime V17 | submit button bij NewUserActivity gemaakt

--- app/src/main/java/java/be/kuleuven/myapplication/LoginActivity.java
+++ app/src/main/java/java/be/kuleuven/myapplication/LoginActivity.java
@@ -9,36 +9,32 @@ import android.widget.Button;
 public class LoginActivity extends Activity {
     // maxime fixt dees
     private UserProfile userProfile;
-    private Button addUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        addUserButton = (Button) findViewById(R.id.newUserButton);
+        // New User button stuff
+        Button addUserButton = (Button) findViewById(R.id.newUserButton);
         addUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activityNewUser();
+                toNewUserActivity();
             }
         });
     }
     // to login en check with the database, then forwards you to the begin screen( the manage screen)
     public void login(String username, String password){
-        // TODO: 2/05/2022 in database kijken of dit account bestaat
-    }
+        // TODO: 2/05/2022 in database kijken of dit account bestaatµ
 
-    public void activityNewUser(){
+
+        //send to MainActivity screen
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    // the NewUser button link to NewUserActivity screen
+    public void toNewUserActivity(){
         Intent intent = new Intent(this, NewUserActivity.class);
         startActivity(intent);
     }
-
-
-    /*
-    Button addUser = (Button) findViewById(R.id.newUserButton);
-        addUser.setOnClickListener(new View.OnClickListener());
-
-     */
-
-
 }
--- app/src/main/java/java/be/kuleuven/myapplication/NewUserActivity.java
+++ app/src/main/java/java/be/kuleuven/myapplication/NewUserActivity.java
@@ -1,7 +1,10 @@
 package java.be.kuleuven.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewUserActivity extends Activity {
     private UserProfile newUser;
@@ -17,6 +20,21 @@ public class NewUserActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
         newUser = new UserProfile("","","","","");
+        Button toMain = (Button) findViewById(R.id.submitButton);
+        toMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toMainActivity();
+            }
+        });
+    }
+
+    private void toMainActivity() {
+        // TODO: 10/05/2022 stream maken van atributen van username, dan nagaan of al de atributen zijn ingevuld,
+        //  zoniet error message, zowel kan op op submit geduuwd worden
+        // TODO: 10/05/2022 (niet belangrijk) zorgen dat eens je op het main screen ben je niet terug kan naar NewUserActivity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     public void setFirstName(String name){
