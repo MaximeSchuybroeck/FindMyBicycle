ef16dd4cf987d7f2337017b5dfbeeda73532bbd3
MaximeSchuybroeck <maxime.schuybroeck@student.kuleuven.be>
2022-05-19 14:05:16 +0200

    V31 |

--- app/src/main/java/java/be/kuleuven/myapplication/EditBikeActivity.java
+++ app/src/main/java/java/be/kuleuven/myapplication/EditBikeActivity.java
@@ -14,36 +14,25 @@ public class EditBikeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_bike);
-        /*
-        @SuppressLint("CutPasteId") TextView tv1 = (TextView)findViewById(R.id.bicycle_name);
-        tv1.setText(app.getEditBike().getName());
-        @SuppressLint("CutPasteId") TextView tv2 = (TextView)findViewById(R.id.editBikeDescription1);
-        tv2.setText((app.getEditBike().getDescription()));
-        */
 
-        /*
-        Button editUsernameButton = findViewById(R.id.editUsername);
-        AutoCompleteTextView userNameEdit = findViewById(R.id.autoCompleteTextView);
-        userNameEdit.setText(app.getUser().getUserName());
-        editUsernameButton.setOnClickListener(v -> editUsername(userNameEdit.getText().toString()));
-         */
         //changeName
         Button editNameButton = (Button) findViewById(R.id.editBikeName);
         AutoCompleteTextView nameEdit = findViewById(R.id.bicycle_name);
-        //nameEdit.setText(app.getEditBike().getName());
+        nameEdit.setText(app.getEditBike().getName(), true);
         editNameButton.setOnClickListener(v -> changeName(nameEdit.getText().toString()));
 
 
         //changeDescription
         Button editDescription = (Button) findViewById(R.id.editBikeDescription);
         AutoCompleteTextView descriptionEdit = findViewById(R.id.editBikeDescription1);
+        nameEdit.setText(app.getEditBike().getDescription(), true);
         editDescription.setOnClickListener(v -> changeDescription(descriptionEdit.getText().toString()));
     }
     @SuppressLint("SetTextI18n")
     private void changeName(String name) {
         TextView tv3 = (TextView)findViewById(R.id.actionMessage);
         if(!name.isEmpty() || !name.equals(app.getEditBike().getName())){
-            app.getEditBike().setName(name);
+            //app.getEditBike().setName(name);
             tv3.setText("SUCCEEDED");
         } else{
             tv3.setText("FAILED");
@@ -53,7 +42,7 @@ public class EditBikeActivity extends Activity {
     private void changeDescription(String description) {
         TextView tv3 = (TextView)findViewById(R.id.actionMessage);
         if(!description.isEmpty() || !description.equals(app.getEditBike().getDescription())){
-            app.getEditBike().setDescription(description);
+            //app.getEditBike().setDescription(description);
             tv3.setText("SUCCEEDED");
         } else{
             tv3.setText("FAILED");
