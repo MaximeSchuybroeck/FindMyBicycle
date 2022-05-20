51f7655edb9715810d5bbcd417f6165479feb4a5
MaximeSchuybroeck <maxime.schuybroeck@student.kuleuven.be>
2022-05-20 13:08:38 +0200

    V34 | edit bike is klaar

--- app/src/main/java/java/be/kuleuven/myapplication/EditBikeActivity.java
+++ app/src/main/java/java/be/kuleuven/myapplication/EditBikeActivity.java
@@ -13,32 +13,17 @@ public class EditBikeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_bike);
 
-        //changeName
-        Button editNameButton = (Button) findViewById(R.id.editBikeName);
-        AutoCompleteTextView nameEdit = findViewById(R.id.bicycle_name);
-        nameEdit.setText(App.getEditBike().getName());
-        editNameButton.setOnClickListener(v -> changeName(nameEdit.getText().toString()));
-
         //changeDescription
         Button editDescription = (Button) findViewById(R.id.editBikeDescription);
-        AutoCompleteTextView descriptionEdit = findViewById(R.id.editBikeDescription1);
-        nameEdit.setText(App.getEditBike().getDescription());
+        AutoCompleteTextView descriptionEdit = findViewById(R.id.bicycle_description);
+        descriptionEdit.setText(App.getEditBike().getDescription());
         editDescription.setOnClickListener(v -> changeDescription(descriptionEdit.getText().toString()));
     }
-    @SuppressLint("SetTextI18n")
-    private void changeName(String name) {
-        TextView tv3 = (TextView)findViewById(R.id.actionMessage);
-        if(!name.isEmpty() || !name.equals(App.getEditBike().getName())){
-            App.getEditBike().setName(name);
-            tv3.setText("SUCCEEDED");
-        } else{
-            tv3.setText("FAILED");
-        }
-    }
+
     @SuppressLint("SetTextI18n")
     private void changeDescription(String description) {
         TextView tv3 = (TextView)findViewById(R.id.actionMessage);
-        if(!description.isEmpty() || !description.equals(App.getEditBike().getDescription())){
+        if(!description.isEmpty() && !description.equals(App.getEditBike().getDescription())){
             App.getEditBike().setDescription(description);
             tv3.setText("SUCCEEDED");
         } else{
