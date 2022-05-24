02ce5da554dca6193ed9566966fa16033ba2add3
LAPTOP-KOENRAAD\koenr <koenraad.goddefroy@hotmail.com>
2022-05-24 21:15:34 +0200

    V38 edit bike volledig oke

--- app/src/main/java/java/be/kuleuven/myapplication/EditBikeActivity.java
+++ app/src/main/java/java/be/kuleuven/myapplication/EditBikeActivity.java
@@ -2,7 +2,6 @@ package java.be.kuleuven.myapplication;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.VoiceInteractor;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.AutoCompleteTextView;
@@ -25,7 +24,7 @@ public class EditBikeActivity extends Activity {
     private RequestQueue requestQueue2;
     private RequestQueue requestQueue3;
     private RequestQueue requestQueue4;
-    private String username, bikeNumber,bikeIdFromDb, bikeDescription, longitude, latitude ;
+    private String username, bikeNumber, bikeDescription, bikeIdFromDb, longitude, latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +44,6 @@ public class EditBikeActivity extends Activity {
             //gets the id of the bike that needs to be deleted
             requestQueue = Volley.newRequestQueue(this);
             String requestURL = "https://studev.groept.be/api/a21pt112/getBikeId";
-
-
-
             requestURL = requestURL + "/" + App.getUser().getUserName() + "/" + App.getEditBike().getNumber();
             StringRequest submitRequest = new StringRequest(Request.Method.GET, requestURL,
                     new Response.Listener<String>() {
@@ -58,18 +54,10 @@ public class EditBikeActivity extends Activity {
                                 JSONObject currentJSonObject = responseArray.getJSONObject(0);
                                 bikeIdFromDb = currentJSonObject.getString("bike_id");
                                 App.setToBeDeletedBikeId(bikeIdFromDb);
-
-                                //get needed data to make new bike after it's edited
                                 username = App.getUser().getUserName();
-                                bikeNumber = App.getEditBike().getNumber();
                                 bikeDescription = App.getEditBike().getDescription();
-                                //bike id is retrieved from db in query above
-
-                                //method makes connection to Db and gets the location of the bike we're gonna change
+                                bikeNumber = App.getEditBike().getNumber();
                                 getLocationFromDb();
-
-
-
                             } catch (Exception e) {
                                 Log.e("database", e.getMessage(), e);
                             }
@@ -81,7 +69,6 @@ public class EditBikeActivity extends Activity {
                             System.out.println(error.getLocalizedMessage());
                         }
                     }
-
             );
             requestQueue.add(submitRequest);
         } else{
@@ -89,6 +76,68 @@ public class EditBikeActivity extends Activity {
             tv3.setText("FAILED");
         }
 
+    }
+    public void secondQueryDeleteBike(){
+        //deletes the old bike
+        requestQueue2 = Volley.newRequestQueue(this);
+        String requestURL2 = "https://studev.groept.be/api/a21pt112/deleteBike";
+        requestURL2 = requestURL2 + "/" + App.getToBeDeletedBikeId();
+        System.out.println(requestURL2);
+        StringRequest submitRequest2 = new StringRequest(Request.Method.GET, requestURL2,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray responseArray = new JSONArray(response);
+                            System.out.println("####################### test 2 delete old bike ");
+                            // adds the ajusted bike
+                            lastQueryAddLocationToBike();
+                            //thirdQueryAddBike();
+                        } catch (JSONException e) {
+                            Log.e("database", e.getMessage(), e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.getLocalizedMessage());
+                    }
+                }
+        );
+        requestQueue2.add(submitRequest2);
+    }
+
+    public void thirdQueryAddBike(){
+        // adds the ajusted bike
+        requestQueue3 = Volley.newRequestQueue(this);
+        String requestURL3 = "https://studev.groept.be/api/a21pt112/addBike";
+        requestURL3 = requestURL3 + "/" + App.getUser().getUserName() + "/" + App.getEditBike().getDescription() + "/" + App.getEditBike().getNumber();
+        StringRequest submitRequest3 = new StringRequest(Request.Method.GET, requestURL3,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray responseArray = new JSONArray(response);
+                            TextView tv3 = (TextView)findViewById(R.id.actionMessage);
+                            tv3.setText("SUCCEEDED");
+                            System.out.println("####################### test 3 addNewbike ");
+                            //lastQueryAddLocationToBike();
+                        } catch (JSONException e) {
+                            Log.e("database", e.getMessage(), e);
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        System.out.println(error.getLocalizedMessage());
+                    }
+                }
+        );
+        requestQueue3.add(submitRequest3);
     }
 
     private void getLocationFromDb() {
@@ -122,74 +171,6 @@ public class EditBikeActivity extends Activity {
         requestQueue4.add(submitRequest3);
     }
 
-
-
-    public void secondQueryDeleteBike(){
-        //deletes the old bike
-        requestQueue2 = Volley.newRequestQueue(this);
-        String requestURL2 = "https://studev.groept.be/api/a21pt112/deleteBike";
-        requestURL2 = requestURL2 + "/" + App.getToBeDeletedBikeId();
-        System.out.println(requestURL2);
-        StringRequest submitRequest2 = new StringRequest(Request.Method.GET, requestURL2,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONArray responseArray = new JSONArray(response);
-
-                            // adds the adjusted bike
-                            thirdQueryAddBike();
-                        } catch (JSONException e) {
-                            Log.e("database", e.getMessage(), e);
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println(error.getLocalizedMessage());
-                    }
-                }
-        );
-        requestQueue2.add(submitRequest2);
-    }
-
-    public void thirdQueryAddBike(){
-        // adds the adjusted bike
-        requestQueue3 = Volley.newRequestQueue(this);
-        String requestURL3 = "https://studev.groept.be/api/a21pt112/addBike";
-        requestURL3 = requestURL3 + "/" + App.getUser().getUserName() + "/" + App.getEditBike().getDescription() + "/" + App.getEditBike().getNumber();
-
-
-
-        StringRequest submitRequest3 = new StringRequest(Request.Method.GET, requestURL3,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONArray responseArray = new JSONArray(response);
-                            TextView tv3 = (TextView)findViewById(R.id.actionMessage);
-                            tv3.setText("SUCCEEDED");
-                            System.out.println("####################### test 3 addNewbike ");
-                            //adds old location to the bike
-                            lastQueryAddLocationToBike();
-                        } catch (JSONException e) {
-                            Log.e("database", e.getMessage(), e);
-                        }
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error)
-                    {
-                        System.out.println(error.getLocalizedMessage());
-                    }
-                }
-        );
-        requestQueue3.add(submitRequest3);
-    }
-
     private void lastQueryAddLocationToBike() {
         requestQueue4 = Volley.newRequestQueue(this);
         String requestURL4 = "https://studev.groept.be/api/a21pt112/addLocation";
@@ -201,6 +182,8 @@ public class EditBikeActivity extends Activity {
                     public void onResponse(String response) {
                         try {
                             JSONArray responseArray = new JSONArray(response);
+                            TextView tv3 = (TextView)findViewById(R.id.actionMessage);
+                            tv3.setText("SUCCEEDED");
                         } catch (JSONException e) {
                             Log.e("database", e.getMessage(), e);
                         }
@@ -212,8 +195,6 @@ public class EditBikeActivity extends Activity {
                         System.out.println(error.getLocalizedMessage());
                     }
                 });
-                requestQueue4.add(stringRequest);
+        requestQueue4.add(stringRequest);
     }
-
-
 }
